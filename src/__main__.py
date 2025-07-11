4736f7f23e6c5808e944f166906153c17923bbed
xefiry <188508735+xefiry@users.noreply.github.com>
2025-07-11 22:57:15 +0200

    simplified main

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,5 @@
-from . import database
-
-# from gui.old import GUI
-from . import gui
+from . import database, gui
 
 if __name__ == "__main__":
     database.init()
-
     gui.start()
-    # gui = GUI()
-    # gui.mainloop()
